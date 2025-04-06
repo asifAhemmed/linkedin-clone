b8384c7a7cd80cd6eaafe265d6aa956a1c5897f8
asif ahemmed <asifahemmed0@gmail.com>
2025-04-06 12:03:40 +0600

    protected-routes-created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,49 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/layout";
 import Home from "./pages/Home";
 import SignUpPage from "./pages/auth/SignUpPage";
 import LoginPage from "./pages/auth/LoginPage";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios";
 
 const App = () => {
   const { data: authUser, isLoading } = useQuery({
-		queryKey: ["authUser"],
-		queryFn: async () => {
-			try {
-				const res = await axiosInstance.get("/auth/me");
-				return res.data;
-			} catch (err) {
-				if (err.response && err.response.status === 401) {
-					return null;
-				}
-				toast.error(err.response.data.message || "Something went wrong");
-			}
-		},
-	});
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await axiosInstance.get("/auth/me");
+        return res.data;
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          return null;
+        }
+        toast.error(err.response.data.message || "Something went wrong");
+      }
+    },
+  });
 
-	if (isLoading) return null;
+  if (isLoading) return null;
 
-  return <Layout>
-    <Routes>
-       <Route path="/" element={<Home/>}/>
-       <Route path="/signup" element={<SignUpPage/>}/>
-       <Route path="/login" element={<LoginPage/>}/>
-    </Routes>
-    <Toaster/>
-  </Layout>;
+  return (
+    <Layout>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+      <Toaster />
+    </Layout>
+  );
 };
 
 export default App;
